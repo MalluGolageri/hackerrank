120981b83ba9b9d44a2cd88032c88917af603d83
Sharanappa Golageri, Mallappa <Mallappa.SharanappaGolageri@cdk.com>
2017-07-09 10:42:06 +0530

    triplets compare in java

--- commitGithub.sh
+++ commitGithub.sh
@@ -4,21 +4,22 @@ git init;
 git add .;
 echo "enter commit message";
 read commitMessage;
-git commit -m $commitMessage;
-enter "remote repository URL";
+echo "commit Message:$commitMessage"
+git commit -m "$commitMessage";
+echo "enter remote repository URL";
 read remoteRepositoryUrl;
 git remote add origin $remoteRepositoryUrl;
 
-
-    read -p "do you want to set new remote to the above entered url? Y/Yes or N/No?" option;
-    case $option in 
-        [Yy][YES][yes] ) git remote -v; break;;
-        [Nn][NnOo] ) break;;
+read -p "do you want to set new remote to the above entered url? Y/Yes or N/No?" option;
+case $option in 
+        [Yy][yes] ) git remote -v; break;;
+        [Nn]) break;;
         * ) echo "Please answer yes or no.";;
-    esac
+esac
 
 echo "pushing to master";
 git push -u origin master
+
 }
 
-commitToGithub();
+commitToGithub;
